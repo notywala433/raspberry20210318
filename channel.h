f8fa5b644aab4537d3c1f626bf9568021501e624
Thami <notywala@yahoo.com>
2021-03-24 19:28:19 +0200

    Refactoring

--- channel.h
+++ channel.h
@@ -6,23 +6,23 @@
 class Channel : public QObject
 {
     Q_OBJECT
-public:
+public          :
     explicit Channel(QObject *parent = nullptr);
 
 
-        bool isOccupied();
-        void setHex(QByteArray h);
-        QByteArray getHex();
-        void setNumber(int a);
-        int getNumber();
-signals:
+        bool            isOccupied();
+        void            setHex(QByteArray h);
+        QByteArray      getHex();
+        void            setNumber(int a);
+        int             getNumber();
+signals         :
 
-public slots:
-private:
-        bool *opened;
-        QByteArray *hex;
-        int *number;
-        bool *occupation;
+public slots    :
+private         :
+                         bool *opened;
+                         QByteArray *hex;
+                         int *number;
+                         bool *occupation;
 
 
 };
--- relay.h
+++ relay.h
@@ -9,24 +9,58 @@ class Relay : public QObject
     Q_OBJECT
 public:
     explicit Relay(QObject *parent = nullptr);
-   Q_INVOKABLE  void openAllChannels();
-     Q_INVOKABLE  void openChannel(int a);
-    Q_INVOKABLE QStringList availablePorts();
-     Q_INVOKABLE  void setPort(QString a);
-    Q_INVOKABLE void chargeAllOn();
-    Q_INVOKABLE void chargeCabinetOn(int a);
-    Q_INVOKABLE void chargeCabinetOff(int a);
-    Q_INVOKABLE void chargeAllOff();
-    Q_INVOKABLE QString getLocalip();
 
-    void (consoleprint)(void);
+ //Open All Channels
+
+ Q_INVOKABLE    void openAllChannels();
+
+ //Open an individual Channel
+
+ Q_INVOKABLE    void openChannel(int a);
+
+ //List All available ports
+
+ Q_INVOKABLE    QStringList availablePorts();
+
+ //Set port to be used to speak to the relay board
+
+ Q_INVOKABLE    void setPort(QString a);
+
+ //Charge All devices on cabinets(Cellphone charging cabinets)
+
+ Q_INVOKABLE    void chargeAllOn();
+
+ //Charge a specific cabinet
+
+ Q_INVOKABLE    void chargeCabinetOn(int a);
+
+ //Switch off Charging on a cabinet
+
+ Q_INVOKABLE    void chargeCabinetOff(int a);
+
+ //Switch off charging on all
+
+ Q_INVOKABLE    void chargeAllOff();
+
+ //Get machines local ip
+
+ Q_INVOKABLE    QString getLocalip();
+
+ //
+
+ void (consoleprint)(void);
     QSerialPort* getSerialport();
+
 signals:
 
 public slots:
-    void printSerialData(QByteArray a);
+
+ //Get incoming data from the serial port
+
+ void printSerialData(QByteArray a);
 
 private:
+
     QSerialPort *port;
     QSerialPortInfo *info;
     Channel channel[25];
